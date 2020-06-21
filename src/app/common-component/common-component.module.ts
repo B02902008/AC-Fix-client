7b3ca8cbb0d2dde1108357966f3cb4b2d94f75d2
SE2019Group1 <r07922037@ntu.edu.tw>
2020-06-21 13:10:36 +0800

    Update common component module file

--- src/app/common-component/common-component.module.ts
+++ src/app/common-component/common-component.module.ts
@@ -2,18 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { CardComponent } from './card/card.component';
+import { TableComponent } from './table/table.component';
+import { RelativeDatePipe } from './relative-date.pipe';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    CardComponent
+    CardComponent,
+    TableComponent,
+    RelativeDatePipe
   ],
-  imports: [
-    CommonModule,
-    CollapseModule
-  ],
+    imports: [
+        CommonModule,
+        CollapseModule,
+        RouterModule
+    ],
   exports: [
-    CardComponent
+    CardComponent,
+    TableComponent,
+    RelativeDatePipe
   ]
 })
 export class CommonComponentModule { }
