59a4c8431e74828ffdee0804449564f83fcc82b9
SE2019Group1 <r07922037@ntu.edu.tw>
2020-06-22 20:40:03 +0800

    Add pagination to table. TODO: sorting

--- src/app/common-component/table/table.component.ts
+++ src/app/common-component/table/table.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ColumnType, ColumnConfig, Pagination } from './table-interface';
+import {PageChangedEvent} from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-table',
@@ -10,10 +11,14 @@ export class TableComponent implements OnInit {
   @Input() columns: ColumnConfig[] = [];
   @Input() enableRowRouting = false;
   @Input() enablePagination = false;
+  @Input() enableJumpToPage = false;
   @Input() pagination: Pagination;
   @Input() dataRows: any[] = [];
+  @Output() loadPageRequest = new EventEmitter<number>();
+  jumpToPageInput: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.columns.forEach(column => {
@@ -21,6 +26,18 @@ export class TableComponent implements OnInit {
       if (!column.sortable) { column.sortable = false; }
       if (!column.center) { column.center = false; }
     });
+  }
+
+  jumpToPage(): void {
+    const page: number = parseInt(this.jumpToPageInput, 10);
+    if (!isNaN(page) && page > 0 ) {
+      this.jumpToPageInput = '';
+      this.pagination.currentPage = Math.min(this.pagination.totalPage, page);
+    }
+  }
+
+  tablePageChanged(event: PageChangedEvent): void {
+    this.loadPageRequest.emit(event.page);
   }
 
 }
