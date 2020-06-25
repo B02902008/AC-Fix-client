cbc487f25cb805ca2a0eec79857877dd85a9f20f
SE2019Group1 <r07922037@ntu.edu.tw>
2020-06-25 19:09:23 +0800

    Adjust global interface and const, and some minor modify

--- src/app/app-interface-and-const.ts
+++ src/app/app-interface-and-const.ts
@@ -1,3 +1,5 @@
+import { MatchingToken, TokenType } from './common-component/terminal-style-log-display/terminal-interface';
+
 export interface AutofixFixingRecord {
   id: number;
   stat: number;
@@ -22,3 +24,38 @@ export const warningTheme = { code: '#FFC107' };
 export const infoTheme = { code: '#63C2DE' };
 export const lightTheme = { code: '#F0F3F5' };
 export const darkTheme = { code: '#2F353A' };
+
+const statIconClass = {
+  0:       { fa: true, 'fa-hourglass-half': true,  'fa-circle-o': false, 'fa-remove': false, 'fa-question': false },
+  1:       { fa: true, 'fa-hourglass-half': false, 'fa-circle-o': true,  'fa-remove': false, 'fa-question': false },
+  '-1':    { fa: true, 'fa-hourglass-half': false, 'fa-circle-o': false, 'fa-remove': true,  'fa-question': false },
+  default: { fa: true, 'fa-hourglass-half': false, 'fa-circle-o': false, 'fa-remove': false, 'fa-question': true  },
+  contains: (k) => (k === 0 || k === 1 || k === -1)
+};
+export const statIconClassLambda = (s: number) => (statIconClass.contains(s) ? statIconClass[s] : statIconClass.default);
+
+const statIconColor = {
+  0: infoTheme.code,
+  1: successTheme.code,
+  '-1': dangerTheme.code,
+  default: darkTheme.code,
+  contains: (k) => (k === 0 || k === 1 || k === -1)
+};
+export const statIconColorLambda = (s: number) => (statIconColor.contains(s) ? statIconColor[s] : statIconColor.default);
+
+export const terminalColoringPattern: MatchingToken[] = [
+  { regex: /(\[)/, type: TokenType.plaintext },
+  { regex: /(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})/, type: TokenType.timestamp },
+  { regex: /(]\[)/, type: TokenType.plaintext },
+  { regex: /(\w{4,5})/, type: TokenType.logLevels },
+  { regex: /( ?] .+)$/, type: TokenType.plaintext }
+];
+
+export const terminalLogLevelColoringStrategy = {
+  start: primaryTheme.code,
+  stage: successTheme.code,
+  final: primaryTheme.code,
+  debug: warningTheme.code,
+  error: dangerTheme.code,
+  info: infoTheme.code
+};
--- src/app/common-component/table/table.component.ts
+++ src/app/common-component/table/table.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ColumnType, ColumnConfig, Pagination, Sorting } from './table-interface';
-import {PageChangedEvent} from 'ngx-bootstrap/pagination';
+import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-table',
